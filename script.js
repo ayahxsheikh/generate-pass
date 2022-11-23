cfeb8644deeecffd991d0b5d9896dbfad60480d4
cc-aeya <aaminah.x@icloud.com>
2022-11-23 20:25:04 +0000

    added math.random func template

--- script.js
+++ script.js
@@ -90,27 +90,28 @@ var upperCasedCharacters = [
 ];
 
 // Function to prompt user for password options
-function getPasswordOptions() {  
-  var passLength = prompt('How many characters would you like your password to contain?'); 
-  
-    if (passLength < 10 || passLength > 64 || passLength == NaN)  {
-      alert('Enter numerical value between 10 - 64 characters'); 
-     return
-    } 
-
-    var lowerC = confirm ('Password will include a lower cased character');
-    var upperC = confirm ('Password will include an upper-cased character');
-    var number = confirm ('Password will include a number');
-    var specialC = confirm ('Password will include a special character');
-  
+  function getPasswordOptions() {  
+    var passLength = prompt('How many characters would you like your password to contain?'); 
     
-  }
+      if (passLength < 10 || passLength > 64 || passLength == NaN)  {
+        alert('Enter numerical value between 10 - 64 characters'); 
+      return
+      } 
 
+      var lowerC = confirm ('Password will include a lower cased character');
+      var upperC = confirm ('Password will include an upper-cased character');
+      var number = confirm ('Password will include a number');
+      var specialC = confirm ('Password will include a special character');
+
+    }
 
 // Function for getting a random element from an array
-function getRandom(arr) {
+  function getRandom(arr) {
+    var randomIndex = Math.floor(Math.random() * arr.length);
+    var indexEl = arr[randomIndex];
 
-}
+    return indexEl;
+  }
 
 // Function to generate password with user input
 function generatePassword() {
